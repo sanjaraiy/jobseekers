3c22fea5aede9caa4d553d6a823f1e28b2a8b274
sanjaraiy <deepakrai78788@gmail.com>
2024-09-14 19:36:34 +0530

    added userSchema

---
+++ backend/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: [true, 'Please provide full Name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide valid email address'],
+        unique: true,
+    },
+    phone: {
+        type: Number,
+        required: [true, 'Please provide your phone number']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide password']
+    },
+    role: {
+        type: String,
+        enum: ['STUDENT','RECRUITER'],
+        required: true,
+    },
+    profile: {
+        bio: {type: String},
+        skills: [{type: String}],
+        resume: {type: String},  //URL to resume file
+        resumeOriginalName: {type: String},
+        company: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Company',
+        },
+        profilePhoto: {
+            type: String,
+            default: "",
+        }
+    }
+}, {timestamps:true});
+
+
+export const User = mongoose.model('User', userSchema);
