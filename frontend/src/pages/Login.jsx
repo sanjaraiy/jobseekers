6aee5f9bd27eb7c872ff271544628a9733aa70c9
sanjaraiy <deepakrai78788@gmail.com>
2024-09-25 23:26:39 +0530

    added done login page and signup page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="flex items-center justify-center max-7-xl mx-auto">
+      <form
+        action=""
+        className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+      >
+        <h1 className="font-bold text-3xl mb-5">Sign in</h1>
+        
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input className="my-2" type="email" placeholder="john@gmail.com" />
+        </div>
+        
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input className="my-2" type="password" placeholder="*******" />
+        </div>
+        <div className="mt-5 flex justify-between items-center">
+          <RadioGroup className="flex items-center gap-5" defaultValue="option-one">
+            
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="APPLICANT" name='role' id="applicant" />
+              <Label htmlFor="applicant">Applicant</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="RECRUITER" name='role' id="recruiter" />
+              <Label htmlFor="recruiter">Recruiter</Label>
+            </div>
+          </RadioGroup>
+        </div>
+        
+        <Button type="submit" className='w-full my-4'>Login</Button>
+        <span className="text-sm">Don't have an account? <Link to={'/signup'} className="text-blue-600 hover:underline">Signup</Link></span>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,6 +3,8 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 function Signup() {
   return (
@@ -28,19 +30,32 @@ function Signup() {
           <Label htmlFor="password">Create Password</Label>
           <Input className="my-2" type="password" placeholder="*******" />
         </div>
-        <div className="mt-5">
-          <RadioGroup className="flex items-center gap-2" defaultValue="option-one">
+        <div className="mt-5 flex justify-between items-center">
+          <RadioGroup className="flex items-center gap-5" defaultValue="option-one">
             
             <div className="flex items-center space-x-2">
-              <RadioGroupItem value="APPLICANT" id="applicant" />
+              <RadioGroupItem value="APPLICANT" name='role' id="applicant" />
               <Label htmlFor="applicant">Applicant</Label>
             </div>
             <div className="flex items-center space-x-2">
-              <RadioGroupItem value="RECRUITER" id="recruiter" />
+              <RadioGroupItem value="RECRUITER" name='role' id="recruiter" />
               <Label htmlFor="recruiter">Recruiter</Label>
             </div>
           </RadioGroup>
+          
+          <div className="flex  items-center gap-2">
+            <Label>Profile</Label>
+            <Input 
+              accept="image/*"
+              type="file"
+              className="cursor-pointer"
+            ></Input>
+          </div>
+          
         </div>
+        
+        <Button type="submit" className='w-full my-4'>Signup</Button>
+        <span className="text-sm">Already have an account? <Link to={'/login'} className="text-blue-600 hover:underline">Login</Link></span>
       </form>
     </div>
   );
