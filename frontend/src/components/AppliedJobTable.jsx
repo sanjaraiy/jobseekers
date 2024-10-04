40e2d0681299b5d337e3dd272613b3375ea6b756
sanjaraiy <deepakrai78788@gmail.com>
2024-10-04 21:47:14 +0530

    added done complete profile

---
+++ frontend/src/components/AppliedJobTable.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "./ui/badge";
+
+function AppliedJobTable() {
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your applied jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+            {
+                [1,2].map((item, idx) => (
+                    <TableRow key={idx}>
+                        <TableCell>17-07-2024</TableCell>
+                        <TableCell>Frontend Developer</TableCell>
+                        <TableCell>Google</TableCell>
+                        <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
+                    </TableRow>
+                ))
+            }
+         
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+export default AppliedJobTable;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,17 +1,22 @@
+import AppliedJobTable from "@/components/AppliedJobTable";
 import Navbar from "@/components/Navbar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 import { Contact, Mail, Pen } from "lucide-react";
 import React from "react";
 import { useSelector } from "react-redux";
 
+const skills = ["HTML", "CSS", "JavaScript", "Reactjs"];
+
 function Profile() {
 //   const { user } = useSelector((store) => store.user);
+const isResume = true;
   return (
     <div>
       <Navbar></Navbar>
-      <div className="max-w-7xl  mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
+      <div className="max-w-4xl  mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
         <div className="flex justify-between">
         <div className="flex items-center gap-4">
           <Avatar className="h-24 w-24">
@@ -44,15 +49,26 @@ function Profile() {
             </div>
           
         </div>
-        <div>
+        <div className="my-5">
             <h1>Skills</h1>
-            <div>
+            <div className="flex items-center gap-1">
                 {
-                    [1,2,4,5].map((item, idx) => <Badge key={idx}>item</Badge>)
+                   skills.length > 0 ? skills.map((item, idx) => <Badge key={idx}>{item}</Badge>) : (<span>Not Applicable</span>)
                 }
             </div>
         </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+           <Label className="text-md font-bold">Resume</Label>
+            {
+              isResume ? <a target="_blank" href="https://youtube.com/@patelmernstack" className="text-blue-500 w-full hover:underline cursor-pointer">Sanjay Rai</a> : <span>Not Applicable</span>
+            }
+        </div>
       </div>
+        <div className="max-w-4xl mx-auto bg-white rounded-2xl">
+          <h1 className="font-bold text-lg my-5">Applied Jobs</h1>
+          {/* Application Table  */}
+          <AppliedJobTable></AppliedJobTable>
+        </div>
     </div>
   );
 }
